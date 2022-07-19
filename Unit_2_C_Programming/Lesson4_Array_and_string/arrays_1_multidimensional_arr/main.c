3f72cf9286fcbff2407d4ba5e37aae54b44c6f55
Ali Mohamed Taima <es-ali.mohamed2019@alexu.edu.eg>
2022-07-19 14:36:32 +0200

    use location instead of index

--- Unit_2_C_Programming/Lesson4_Array_and_string/arrays_1_multidimensional_arr/main.c
+++ Unit_2_C_Programming/Lesson4_Array_and_string/arrays_1_multidimensional_arr/main.c
@@ -16,7 +16,7 @@ int main()
 	{
 		for(j = 0; j<2; j++)
 		{
-			printf("Enter a%d%d: ", i, j);
+			printf("Enter a%d%d: ", i+1, j+1);
 			fflush(stdin);fflush(stdout);
 			scanf(" %f",&a[i][j]);
 
@@ -28,7 +28,7 @@ int main()
 	{
 		for(j = 0; j<2; j++)
 		{
-			printf("Enter b%d%d: ", i, j);
+			printf("Enter b%d%d: ", i+1, j+1);
 			fflush(stdin);fflush(stdout);
 			scanf(" %f",&b[i][j]);
 
