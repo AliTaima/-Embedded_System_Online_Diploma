834f092922d78716d370dfcefd868f388c6dbe8a
Ali Mohamed Taima <es-ali.mohamed2019@alexu.edu.eg>
2022-08-07 10:39:18 +0200

    new solution with xor (short and very good)

--- Unit_2_C_Programming/Midterm_codes/Q6_ret_unique_num_in_arr_1_loop/main.c
+++ Unit_2_C_Programming/Midterm_codes/Q6_ret_unique_num_in_arr_1_loop/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-int unique_num(int arr[], int size, int index);
+int unique_num(int arr[], int size);
 void scan_arr(int arr[], int size);
 int flag; // global variable used to check that the array end and not found unique number or not
 int main()
@@ -9,37 +9,20 @@ int main()
     printf("Please enter the size of your array: \n");
     scanf("%d", &size);
     scan_arr(arr, size);
-    printf("the unique number = %d", unique_num(arr, size, 0));
+    printf("the unique number = %d", unique_num(arr, size));
     return 0;
 }
 
-int unique_num(int arr[], int size, int index)
+int unique_num(int arr[], int size)
 {
-    // This function finds the unique number in the given array by recursion and one for loop
-    int a;
-    for(a = 0; a<size; a++)
-    {
-        // Escape comparing the same element
-        if(a == index)
-            continue;
-        // checking if the current element equal to any element in the array or not
-        if(arr[a] == arr[index])
-            {
-                flag = 1;
-                break;
-            }
-    }
-    // checking the end or the array and decide that the array has unique number or not
-    if((index == (size -1)) && (flag == 1) && (a!=size))
-    {
-        printf("There is no unique number in the array");
-        exit(1);
-    }
-
-    if(a == size || index == (size -1) )
-        return arr[index];
-    else
-        unique_num(arr, size, index +1);
+    /*
+        The idea here understanding the xor operator that return 0 if the two numbers are equal and return the number
+        that xoring with 0
+    */
+    int res = 0, i;
+    for(i = 0; i<size; i ++)
+        res ^= arr[i];
+    return res;
 }
 
 void scan_arr(int arr[], int size)
