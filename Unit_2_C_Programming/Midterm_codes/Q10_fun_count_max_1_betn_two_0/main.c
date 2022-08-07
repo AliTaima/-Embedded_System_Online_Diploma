9bf17690398a428c4572739b9839aadcaab95ed1
Ali Mohamed Taima <es-ali.mohamed2019@alexu.edu.eg>
2022-08-07 11:09:02 +0200

    simple code with logical operations

--- Unit_2_C_Programming/Midterm_codes/Q10_fun_count_max_1_betn_two_0/main.c
+++ Unit_2_C_Programming/Midterm_codes/Q10_fun_count_max_1_betn_two_0/main.c
@@ -1,45 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-int count_1(int num, int arr[]);
+int count_1(int num);
 int main()
 {
-    int num, arr[100], result;
+    int num, result;
     printf("Input number: ");
     scanf("%d", &num);
-    result = count_1(num, arr);
+    result = count_1(num);
     printf("The maximum number of 1's = %d", result);
     return 0;
 }
 
-int count_1(int num, int arr[])
+int count_1(int num)
 {
-    // This function return the max number of zeros between two 0's in the binary representation of the number
-    int rem, flag = 0, max = 0, count = 0;
-    while(num)
+
+    int count = 0;
+    while(num != 0)
     {
-        rem = num%2;
-        num /= 2;
-        if(rem == 0)
-        {
-            if(max < count)
-            {
-                max = count;
-                flag = 1;
-                /*
-                 I use this flag to make sure that the assigning occur betn max and count
-                 as in some cases we can't get the last zero in the number
-                */
-            }
-
-            count = 0;
-            continue;
-        }
-        if(rem == 1)
-                count ++;
-
+        num = (num &(num << 1));
+        count ++;
     }
-    if(flag == 1)
-        return max;
-    else
-        return count;
+    return count;
 }
