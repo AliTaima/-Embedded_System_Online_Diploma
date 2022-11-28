fcc20092dba6d339c91d84c531ef664f1eb5e546
Ali Mohamed Taima <es-ali.mohamed2019@alexu.edu.eg>
2022-11-28 10:48:12 +0200

    Makefile2
    In this file I use CC, CFLAGS, INCS AND LIBS commands in the Makefile

--- Unit_3_Embedded_C/Lesson3/Codes/app.c
+++ Unit_3_Embedded_C/Lesson3/Codes/app.c
@@ -2,6 +2,7 @@
 
 unsigned char string_buffer[100] = "Learn-in-depth:<AliTaima>";
 unsigned char const string_buffer2[100] = "to create a rodate section";
+unsigned char const string_buffer3[100] = "to create a rodate section";
 void main(void)
 {
 	// Send on physical board
