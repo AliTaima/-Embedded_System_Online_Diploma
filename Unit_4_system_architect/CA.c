7a3a3bb1fb99ef4505efff1f8268b956ce50e253
Ali Mohamed Taima <es-ali.mohamed2019@alexu.edu.eg>
2022-12-24 10:41:12 +0200

    func to generate random distance to test the code

--- Unit_4_system_architect/CA.c
+++ Unit_4_system_architect/CA.c
@@ -21,6 +21,7 @@ STATE_define(CA_waiting)
 
 	//Event_Check
     //US_Get_distance(CA_distance)
+    CA_distance = US_Get_distance_random(45, 55, 1);
 	(CA_distance <= CA_threshold)?(CA_state = STATE(CA_waiting)):(CA_state = STATE(CA_driving));
 	printf("CA_Wainting State: distance = %d Speed = %d \n", CA_distance, CA_speed);
 }
@@ -34,7 +35,21 @@ STATE_define(CA_driving)
     CA_speed = 30;
     //Event_Check
     //US_Get_distance(CA_distance)
+    CA_distance = US_Get_distance_random(45, 55, 1);
+
     (CA_distance <= CA_threshold)?(CA_state = STATE(CA_waiting)):(CA_state = STATE(CA_driving));
     printf("CA_Driving State: distance = %d Speed = %d \n", CA_distance, CA_speed);
 }
 
+int US_Get_distance_random(int l, intr, int count)
+{
+
+    // this wil generate rnadom number in range 1 to r
+    int i;
+    for(i = 0; i < count; i++)
+    {
+        int rand_num = (rand() % (r - 1 + 1)) + 1;
+        return rand_num;
+    }
+    
+}
