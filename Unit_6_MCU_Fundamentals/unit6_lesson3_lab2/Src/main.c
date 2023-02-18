97b6ce74540e0056c6961b9e1434029b7a2a89f3
Ali Mohamed Taima <es-ali.mohamed2019@alexu.edu.eg>
2023-02-18 15:29:11 +0200

    Set different values for clocks
    Configure Board to run with the
    Following rates:
     APB1 Bus frequency 4MHZ
     APB2 Bus frequency 2MHZ
     AHB frequency 8 MHZ
     SysClk 8 MHZ
     Use only internal HSI_RC

--- Unit_6_MCU_Fundamentals/unit6_lesson3_lab2/Src/main.c
+++ Unit_6_MCU_Fundamentals/unit6_lesson3_lab2/Src/main.c
@@ -37,6 +37,8 @@ typedef volatile unsigned int vuint32_t ;
 // RCC register
 #define RCC_BASE 0x40021000
 #define RCC_APB2ENR *(volatile uint32_t *)(RCC_BASE + 0x18)
+// clock configuration register
+#define RCC_CFGR *(volatile uint32_t *)(RCC_BASE + 0x04)
 void init_clock(void)
 {
 	// --> init clock for GPIOA
@@ -45,6 +47,27 @@ void init_clock(void)
 	//	0: IO port A clock disabled
 	//	1: IO port A clock enabled
 	RCC_APB2ENR |= (1<<2);
+	//	-------------------------------------
+	//	Bits 13:11 PPRE2: APB high-speed prescaler (APB2)
+	//	Set and cleared by software to control the division factor of the APB high-speed clock
+	//	(PCLK2).
+	//	0xx: HCLK not divided
+	//	100: HCLK divided by 2
+	//	101: HCLK divided by 4
+	//	110: HCLK divided by 8
+	//	111: HCLK divided by 16
+	RCC_CFGR |= (0b101<<11); // divide by 4
+	//	----------------------------------------------------------
+	//	Bits 10:8 PPRE1: APB low-speed prescaler (APB1)
+	//	Set and cleared by software to control the division factor of the APB low-speed clock
+	//	(PCLK1).
+	//	Warning: the software has to set correctly these bits to not exceed 36 MHz on this domain.
+	//	0xx: HCLK not divided
+	//	100: HCLK divided by 2
+	//	101: HCLK divided by 4
+	//	110: HCLK divided by 8
+	//	111: HCLK divided by 16
+	RCC_CFGR |= (0b100<<8); // divide by 2
 
 }
 int main(void)
