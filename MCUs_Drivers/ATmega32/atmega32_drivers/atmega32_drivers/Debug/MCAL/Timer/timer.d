6c65b658af978aecc4a3e1d60bd5a4139d2fd677
Ali Mohamed Taima <es-ali.mohamed2019@alexu.edu.eg>
2023-05-09 11:33:44 +0300

    Add basic drivers that I made in fwd
    I add the basic drivers that I made in Embedded_Systems_FWD_T2_Professional
    my basic layers are MCAL->ECUAL->APP
    I also added Utilities folder that has registers and types I want to
    define

---
+++ MCUs_Drivers/ATmega32/atmega32_drivers/atmega32_drivers/Debug/MCAL/Timer/timer.d
@@ -0,0 +1,9 @@
+MCAL/Timer/timer.d MCAL/Timer/timer.o: ../MCAL/Timer/timer.c \
+ ../MCAL/Timer/timer.h ../MCAL/Timer/../../Utilities/registers.h \
+ ../MCAL/Timer/../../Utilities/types.h
+
+../MCAL/Timer/timer.h:
+
+../MCAL/Timer/../../Utilities/registers.h:
+
+../MCAL/Timer/../../Utilities/types.h:
---
+++ MCUs_Drivers/ATmega32/atmega32_drivers/atmega32_drivers/Debug/main.d
@@ -0,0 +1,36 @@
+main.d main.o: .././main.c .././Application/application.h \
+ .././Application/../ECUAL/LED/led.h \
+ .././Application/../ECUAL/LED/../../MCAL/Timer/timer.h \
+ .././Application/../ECUAL/LED/../../MCAL/Timer/../../Utilities/registers.h \
+ .././Application/../ECUAL/LED/../../MCAL/Timer/../../Utilities/types.h \
+ .././Application/../ECUAL/LED/../../MCAL/DIO/dio.h \
+ .././Application/../ECUAL/LED/../../MCAL/DIO/../../Utilities/registers.h \
+ .././Application/../ECUAL/LED/../../MCAL/DIO/../../Utilities/types.h \
+ .././Application/../ECUAL/LED/../../MCAL/DIO/../Interrupt_Library/interrupts.h \
+ .././Application/../ECUAL/LED/../../MCAL/DIO/../Interrupt_Library/../DIO/dio.h \
+ .././Application/../ECUAL/Button/button.h \
+ .././Application/../ECUAL/Button/../../MCAL/DIO/dio.h
+
+.././Application/application.h:
+
+.././Application/../ECUAL/LED/led.h:
+
+.././Application/../ECUAL/LED/../../MCAL/Timer/timer.h:
+
+.././Application/../ECUAL/LED/../../MCAL/Timer/../../Utilities/registers.h:
+
+.././Application/../ECUAL/LED/../../MCAL/Timer/../../Utilities/types.h:
+
+.././Application/../ECUAL/LED/../../MCAL/DIO/dio.h:
+
+.././Application/../ECUAL/LED/../../MCAL/DIO/../../Utilities/registers.h:
+
+.././Application/../ECUAL/LED/../../MCAL/DIO/../../Utilities/types.h:
+
+.././Application/../ECUAL/LED/../../MCAL/DIO/../Interrupt_Library/interrupts.h:
+
+.././Application/../ECUAL/LED/../../MCAL/DIO/../Interrupt_Library/../DIO/dio.h:
+
+.././Application/../ECUAL/Button/button.h:
+
+.././Application/../ECUAL/Button/../../MCAL/DIO/dio.h:
